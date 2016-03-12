a9423253579c44ac0b88719c45cb07c0d043a5fa
victorock <root@vicdacosta>
2016-03-12 18:16:29 +0100

    Update

---
+++ sio-lia/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Clean rpm locks before puppet run.
+# See ticket https://bugs.launchpad.net/fuel/+bug/1339236
+rm -f /var/lib/rpm/__db.*
+rpm --rebuilddb
+
+# Run puppet to apply custom config
+systemctl daemon-reload
+
+puppet apply --debug --verbose --color false --detailed-exitcodes \
+/etc/puppet/modules/scaleio2/examples/fuel-sio-lia.pp || [[ $? == 2 ]]
+
+systemctl enable lia.service
+systemctl restart lia.service
